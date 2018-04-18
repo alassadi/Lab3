cd3be03f168f49faa3d06927544b626cff447c9e
Hankern1996 <hannah_kern2007@web.de>
2018-04-18 12:48:36 +0200

    Merge branch 'master' of https://github.com/alassadi/Lab3 into Hannah&Paulina
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/company/lab3/MainActivity.java
+++ app/src/main/java/com/company/lab3/MainActivity.java
@@ -2,13 +2,55 @@ package com.company.lab3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button withdraw;
+    private TextView balance;
+    private EditText withdrawMoney;
+    private Account mAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        balance = (TextView)findViewById(R.id.balance);
+        withdrawMoney = (EditText)findViewById(R.id.edittext);
+
+
+        withdraw = (Button)findViewById(R.id.withdraw);
+
+        withdraw.setOnClickListener( new View.OnClickListener(){
+            public void onClick (View v){
+                updateBalance();
+                //updateDatabase();
+                //Set new balance to Textview from Database
+                //activity refresh the balance/activity
+
+            }
+        });
+
     }
+
+    public double updateBalance(){
+        double balance1 = Double.parseDouble(balance.toString());
+        double withdraw1 = Double.parseDouble(withdrawMoney.toString());
+
+        double sum = balance1 - withdraw1;
+
+        return sum;
+
+    }
+    //update Database with new Balance
+
+
+
+
+
+
 }
